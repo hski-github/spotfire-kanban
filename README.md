7336bc8245d1be62962af209b0909a71bca2fbf7
hski <holger@schimanski-web.de>
2021-07-05 23:10:11 +0200

    Initial commit as copy from js-dev-starter and modifications

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# spotfire-brickwall
-Spotfire Mods Brickwall Visualisation
+# Spotfire Kanban Board Visualisation
+
+Spotfire mods visualisation for Kanban board in Spotfire.
+
+
+## How to get started (with development server)
+All source code for the mod example can be found in the `src` folder. 
+These instructions assume that you have [Node.js](https://nodejs.org/en/) (which includes npm) installed.
+
+- Open a terminal at the location of this example.
+- Run `npm install`. This will install necessary tools. Run this command only the first time you are building the mod and skip this step for any subsequent builds.
+- Run `npm run server`. This will start a development server.
+- Start editing, for example `src/main.js`.
+- In Spotfire, follow the steps of creating a new mod and connecting to the development server.
---
+++ src/main.js
@@ -0,0 +1,65 @@
+/**
+ * Get access to the Spotfire Mod API by providing a callback to the initialize method.
+ * @param {Spotfire.Mod} mod - mod api
+ */
+Spotfire.initialize(async (mod) => {
+    /**
+     * Create the read function.
+     */
+    const reader = mod.createReader(mod.visualization.data(), mod.windowSize());
+
+    /**
+     * Store the context.
+     */
+    const context = mod.getRenderContext();
+
+    /**
+     * Initiate the read loop
+     */
+    reader.subscribe(render);
+
+    /**
+     * @param {Spotfire.DataView} dataView
+     * @param {Spotfire.Size} windowSize
+     * @param {Spotfire.ModProperty<string>} prop
+     */
+    async function render(dataView, windowSize) {
+        /**
+         * Check the data view for errors
+         */
+        let errors = await dataView.getErrors();
+        if (errors.length > 0) {
+            // Showing an error overlay will hide the mod iframe.
+            // Clear the mod content here to avoid flickering effect of
+            // an old configuration when next valid data view is received.
+            mod.controls.errorOverlay.show(errors);
+            return;
+        }
+        mod.controls.errorOverlay.hide();
+
+        /**
+         * Get the hierarchy of the categorical X-axis.
+         */
+        const xHierarchy = await dataView.hierarchy("Column");
+        const xRoot = await xHierarchy.root();
+
+        if (xRoot == null) {
+            // User interaction caused the data view to expire.
+            // Don't clear the mod content here to avoid flickering.
+            return;
+        }
+
+        /**
+         * Print out to document
+         */
+        const container = document.querySelector("#mod-container");
+        container.textContent = `windowSize: ${windowSize.width}x${windowSize.height}\r\n`;
+        container.textContent += `should render: ${xRoot.rows().length} rows\r\n`;
+        container.textContent += `${prop.name}: ${prop.value()}`;
+
+        /**
+         * Signal that the mod is ready for export.
+         */
+        context.signalRenderComplete();
+    }
+});
