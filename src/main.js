f27122046574a9ed0a3eaa03725585b1621a9762
hski <holger@schimanski-web.de>
2022-02-18 20:27:43 +0100

    Use hex 61646B instead of black as font color

--- src/main.js
+++ src/main.js
@@ -235,5 +235,5 @@ function getContrastYIQ(hexcolor){
     var g = parseInt(hexcolor.substr(2,2),16);
     var b = parseInt(hexcolor.substr(4,2),16);
     var yiq = ((r*299)+(g*587)+(b*114))/1000;
-    return (yiq >= 180) ? 'black' : 'white';
+    return (yiq >= 180) ? '#61646B' : 'white';
 }
